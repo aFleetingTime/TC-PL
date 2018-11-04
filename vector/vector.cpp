32381dfd3178b6d8eaf6d8c0045e5f13b3507b3f
Fleeting Time <liunian320@outlook.com>
2018-11-05 00:42:21 +0800

    ../..

--- vector/vector.cpp
+++
@@ -1,8 +0,0 @@
-vector()
-{
-	elem = a.allocate(n);
-	if constexpr(e)
-		space = last = std::uninitializerd_move_n(elem, n, val);
-	else constexpr(e)
-		space = last = std::uninitializerd_copy_n(elem, n, val);
-}
